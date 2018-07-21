cb703beb810bea356ea7c2d7b0768a1910f135ab
Peter Sulucz <peter.sulucz@gmail.com>
2018-07-21 13:22:32 -0700

    Deploy with tagz

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,55 @@
-﻿#!/bin/bash
+#!/bin/bash
+echo "Tags detected $TRAVIS_TAG running deployment"
 
-if [ "$TRAVIS_BUILD_NUMBER" == "" ]; then
-	TRAVIS_BUILD_NUMBER=0
+if [ "$TRAVIS_TAG" != "" ]; then
+
+	echo "Running deployment"
+
+	tagarray=$(echo $TRAVIS_TAG | tr "-" "\n")
+
+	count=0
+	postfix=""
+	for prefix in $tagarray
+	do
+		if [ "$count" == "0" ]; then
+			assemblyversion=$prefix
+		else
+			if [ "$postfix" == "" ]; then
+				postfix="$prefix"
+			else
+				postfix="$postfix-$prefix"
+			fi
+		fi
+		count=$count+1
+	done
+
+	echo "Assembly Version: $assemblyversion"
+	echo "Version Suffix: $postfix"
+
+
+	# Now find all of the directories with a nuspec...
+	NugetProjectFolders=$(find `pwd` -regex .*.*\.nuspec | grep -v '/obj/' | grep -v '/bin/')
+	for nuspec in $NugetProjectFolders
+	do
+		projdir=$(dirname $nuspec)
+
+		echo "Packing project $projdir"
+
+		dotnet pack --version-suffix "'$postfix'" /p:version='$assemblyversion' $projdir
+		if [ $? -ne 0 ]; then
+			exit 1
+		fi
+	done
+
+	# Now find all of the nuget packages.
+	NugetPackages=$(find `pwd` -regex .*Sulucz\..*.*\.nupkg | grep -v '/obj/')
+	for package in $NugetPackages
+	do
+		echo "Pushing package $package"
+		
+		dotnet nuget push --api-key "$NUGET_API_KEY" --source 'https://www.nuget.org' $package
+		if [ $? -ne 0 ]; then
+			exit 1
+		fi
+	done
 fi
-
-AssemblyVersion="1.0.$TRAVIS_BUILD_NUMBER"
-
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-	VersionSuffix=""
-else
-	VersionSuffix="$TRAVIS_PULL_REQUEST_BRANCH"
-fi
-
-echo "Build number: $TRAVIS_BUILD_NUMBER"
-echo "Assembly version $AssemblyVersion"
-echo "Version suffix $VersionSuffix"
-
-# Now find all of the directories with a nuspec...
-NugetProjectFolders=$(find `pwd` -regex .*.*\.nuspec | grep -v '/obj/' | grep -v '/bin/')
-for nuspec in $NugetProjectFolders
-do
-	projdir=$(dirname $nuspec)
-
-	echo "Packing project $projdir"
-
-	dotnet pack --version-suffix "'$VersionSuffix'" /p:version=$AssemblyVersion $projdir
-	if [ $? -ne 0 ]; then
-		exit 1
-	fi
-done
-
-echo "Tags: $TRAVIS_TAG"
-
-# Now find all of the nuget packages.
-NugetPackages=$(find `pwd` -regex .*Sulucz\..*.*\.nupkg | grep -v '/obj/')
-for package in $NugetPackages
-do
-	echo "Pushing package $package"
-	
-	dotnet nuget push --api-key "$NUGET_API_KEY" --source 'https://www.nuget.org' $package
-	if [ $? -ne 0 ]; then
-		exit 1
-	fi
-done
