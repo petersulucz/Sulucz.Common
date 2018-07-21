6dadfef7e99759f17a68d34582349a89969e989b
Peter Sulucz <peter.sulucz@gmail.com>
2018-07-21 12:29:21 -0700

    Adding signing keys

--- deploy.sh
+++ deploy.sh
@@ -31,13 +31,13 @@ do
 done
 
 # Now find all of the nuget packages.
-NugetPackages=$(find `pwd` -regex .*Sulucz\..*.*\.nupkg | grep -v '/obj/')
-for package in $NugetPackages
-do
-	echo "Pushing package $package"
-	
-	dotnet nuget push --api-key "$NUGET_API_KEY" --source 'https://www.nuget.org' $package
-	if [ $? -ne 0 ]; then
-		exit 1
-	fi
-done
+#NugetPackages=$(find `pwd` -regex .*Sulucz\..*.*\.nupkg | grep -v '/obj/')
+#for package in $NugetPackages
+#do
+#	echo "Pushing package $package"
+#	
+#	dotnet nuget push --api-key "$NUGET_API_KEY" --source 'https://www.nuget.org' $package
+#	if [ $? -ne 0 ]; then
+#		exit 1
+#	fi
+#done
