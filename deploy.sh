94980d3f4b7750abb3c623c9d6e8152f840a5bde
Peter Sulucz <peter.sulucz@gmail.com>
2018-07-21 11:50:42 -0700

    come on build

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [ "$TRAVIS_BUILD_NUMBER" == "" ]; then
+	TRAVIS_BUILD_NUMBER=0
+fi
+
 AssemblyVersion="1.0.$TRAVIS_BUILD_NUMBER"
 
 if [ "$TRAVIS_BRANCH" != "master" ]; then
@@ -7,16 +12,21 @@ else
 	VersionSuffix="$TRAVIS_PULL_REQUEST_BRANCH"
 fi
 
+echo "Build number: $TRAVIS_BUILD_NUMBER"
 echo "Assembly version $AssemblyVersion"
 echo "Version suffix $VersionSuffix"
 
 # Now find all of the directories with a nuspec...
 NugetProjectFolders=$(find `pwd` -regex .*.*\.nuspec | grep -v '/obj/' | grep -v '/bin/')
-for project in $NugetProjectFolders
+for nuspec in $NugetProjectFolders
 do
-	dotnet pack --version-suffix "'$VersionSuffix'" /p:version=$AssemblyVersion $project
+	projdir=$(dirname $nuspec)
+
+	echo "Packing project $projdir"
+
+	dotnet pack --version-suffix "'$VersionSuffix'" /p:version=$AssemblyVersion $projdir
 	if [ $? -ne 0 ]; then
-		exit $?
+		exit 1
 	fi
 done
 
@@ -25,8 +35,9 @@ NugetPackages=$(find `pwd` -regex .*Sulucz\..*.*\.nupkg | grep -v '/obj/')
 for package in $NugetPackages
 do
 	echo "Pushing package $package"
+	
 	dotnet nuget push --api-key "$NUGET_API_KEY" --source 'https://www.nuget.org' $package
 	if [ $? -ne 0 ]; then
-		exit $?
+		exit 1
 	fi
 done
