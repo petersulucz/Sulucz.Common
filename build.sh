61997958d1e89b40b7f0920217788792523f50d6
Peter Sulucz <peter.sulucz@gmail.com>
2018-07-21 09:13:23 -0700

    Fixing stuff yo

---
+++ build.sh
@@ -0,0 +1,19 @@
+﻿#!/bin/bash
+projects=$(find `pwd` -regex .*\.csproj$)
+echo "Compiling projects:"
+echo "$projects"
+
+echo "Restoring projects"
+for project in $projects 
+do
+	echo "Restore for $project"
+	dotnet restore $project
+done
+
+echo "Building projects"
+for project in $projects 
+do
+	echo "Build for $project"
+	dotnet build $project
+done
+
