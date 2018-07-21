db8097b745840a2b76b2b1ce66fc57a268370df3
Peter Sulucz <peter.sulucz@gmail.com>
2018-07-21 07:48:04 -0700

    initial commit

---
+++ Code/Sulucz.Common.Datastructures/ExpandableGrid.cs
@@ -0,0 +1,96 @@
+﻿// <copyright file="ExpandableGrid.cs" company="Peter Sulucz">
+// Copyright (c) Peter Sulucz. All rights reserved.
+// </copyright>
+
+namespace Sulucz.Common.Datastructures
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ExpandableGrid<T>
+    {
+        /// <summary>
+        /// The dictionary.
+        /// </summary>
+        private readonly Dictionary<int, Dictionary<int, T>> rows;
+
+        public ExpandableGrid()
+        {
+            this.rows = new Dictionary<int, Dictionary<int, T>>();
+        }
+
+        /// <summary>
+        /// Gets the actual count of items in the matrix.
+        /// </summary>
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// Gets the minimum row in the matrix.
+        /// </summary>
+        public int MinRow { get; private set; }
+
+        /// <summary>
+        /// Gets the maximum row in the matrix.
+        /// </summary>
+        public int MaxRow { get; private set; }
+
+        /// <summary>
+        /// Gets the minimum column in the matrix.
+        /// </summary>
+        public int MinColumn { get; private set; }
+
+        /// <summary>
+        /// Gets the maximum column in the matrix.
+        /// </summary>
+        public int MaxColumn { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the value at a certain index.
+        /// </summary>
+        /// <param name="row">The row.</param>
+        /// <param name="column">The column.</param>
+        /// <returns>The value at the indicies.</returns>
+        public T this[int row, int column]
+        {
+            get
+            {
+                if (true == this.TryGetValue(row, column, out var value))
+                {
+                    return value;
+                }
+
+                throw new KeyNotFoundException($"The item was not found at row = {row} column = {column}.");
+            }
+
+            set
+            {
+                Dictionary<int, T> columns;
+                if (false == this.rows.TryGetValue(row, out columns))
+                {
+                    this.rows[row] = columns = new Dictionary<int, T>();
+                }
+
+                columns[column] = value;
+
+                // Update all variables
+                this.MaxColumn = Math.Max(this.MaxColumn, column);
+                this.MinColumn = Math.Min(this.MinColumn, column);
+                this.MaxRow = Math.Max(this.MaxRow, row);
+                this.MinRow = Math.Min(this.MinRow, row);
+            }
+        }
+
+        public bool TryGetValue(int row, int column, out T value)
+        {
+            Dictionary<int, T> columns;
+            if (true == this.rows.TryGetValue(row, out columns)
+                && true == columns.TryGetValue(column, out value))
+            {
+                return true;
+            }
+
+            value = default(T);
+            return false;
+        }
+    }
+}
---
+++ Test/Sulucz.Common.Datastructures.Tests/ExpandableGridTests.cs
@@ -0,0 +1,118 @@
+// <copyright file="ExpandableGridTests.cs" company="Peter Sulucz">
+// Copyright (c) Peter Sulucz. All rights reserved.
+// </copyright>
+
+namespace Sulucz.Common.Datastructures.Tests
+{
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ExpandableGridTests
+    {
+        [TestMethod]
+        public void TestInsert()
+        {
+            var grid = new ExpandableGrid<object>();
+            var obj = new object();
+            grid[0, 0] = obj;
+            Assert.AreSame(obj, grid[0, 0]);
+        }
+
+        [TestMethod]
+        public void TestInsert2()
+        {
+            var grid = new ExpandableGrid<object>();
+            var obj = new object();
+            grid[-20000, 100] = obj;
+            Assert.AreSame(obj, grid[-20000, 100]);
+        }
+
+        [TestMethod]
+        public void TestMinColumn()
+        {
+            var grid = new ExpandableGrid<object>();
+            var obj = new object();
+            grid[0, 0] = obj;
+            Assert.AreEqual(0, grid.MinColumn);
+        }
+
+        [TestMethod]
+        public void TestMinColumn2()
+        {
+            const int MinColumn = -200;
+
+            var grid = new ExpandableGrid<object>();
+            var obj = new object();
+            grid[0, MinColumn] = obj;
+            Assert.AreEqual(MinColumn, grid.MinColumn);
+        }
+
+        [TestMethod]
+        public void TestMinColumn3()
+        {
+            const int MinColumn = -200;
+
+            var grid = new ExpandableGrid<object>();
+            var obj = new object();
+            grid[0, MinColumn] = obj;
+            grid[0, MinColumn + 1] = obj;
+            Assert.AreEqual(MinColumn, grid.MinColumn);
+        }
+
+        [TestMethod]
+        public void TestMaxColumn()
+        {
+            const int MaxColumn = 200;
+
+            var grid = new ExpandableGrid<object>();
+            var obj = new object();
+            grid[0, MaxColumn] = obj;
+            Assert.AreEqual(MaxColumn, grid.MaxColumn);
+        }
+
+        [TestMethod]
+        public void TestMaxRpw()
+        {
+            const int MaxRow = 200;
+
+            var grid = new ExpandableGrid<object>();
+            var obj = new object();
+            grid[MaxRow, 0] = obj;
+            Assert.AreEqual(MaxRow, grid.MaxRow);
+        }
+
+        [TestMethod]
+        public void TestMinRpw()
+        {
+            const int MinRow = 200;
+
+            var grid = new ExpandableGrid<object>();
+            var obj = new object();
+            grid[MinRow, 0] = obj;
+            Assert.AreEqual(MinRow, grid.MaxRow);
+        }
+
+        /// <summary>
+        /// Test try get value with an empty set.
+        /// </summary>
+        [TestMethod]
+        public void TestTryGetValueEmpty()
+        {
+            var grid = new ExpandableGrid<object>();
+            var result = grid.TryGetValue(5, 5, out var value);
+            Assert.IsFalse(result);
+            Assert.IsNull(value);
+        }
+
+        /// <summary>
+        /// Test get value empty.
+        /// </summary>
+        [TestMethod]
+        public void TestGetValueEmpty()
+        {
+            var grid = new ExpandableGrid<object>();
+            Assert.ThrowsException<KeyNotFoundException>(() => grid[0, 0]);
+        }
+    }
+}
