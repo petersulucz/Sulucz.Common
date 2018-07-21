5bbde8c3e345533bface822fbde0bcc4e432d38b
Peter Sulucz <peter.sulucz@gmail.com>
2018-07-21 10:56:27 -0700

    adding the deploy script

--- build.sh
+++ build.sh
@@ -16,23 +16,3 @@ do
 	echo "Build for $project"
 	dotnet build $project
 done
-
-AssemblyVersion="1.0.$TRAVIS_BUILD_NUMBER"
-
-if [ "$TRAVIS_BRANCH" != "master"]; then
-	VersionSuffix=""
-else
-	VersionSuffix="$TRAVIS_PULL_REQUEST_BRANCH"
-fi
-
-echo "Assembly version $AssemblyVersion"
-echo "Version suffix $VersionSuffix"
-
-# Now find all of the directories with a nuspec...
-NugetProjectFolders=$(find `pwd` -regex .*.*\.nuspec | grep -v '/obj/' | grep -v '/bin/')
-for project in $NugetProjectFolders
-do
-	nuget pack --version-suffix $VersionSuffix /p:version=$AssemblyVersion
-done
-
-# Now find all of the nuget packages.
