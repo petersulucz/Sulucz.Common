a39437caa26686924b8e5f216e06993ac14ce4d8
Peter Sulucz <peter.sulucz@gmail.com>
2018-08-27 21:48:53 -0700

    Adding changes for stuff

---
+++ Code/Sulucz.Common/ListExtensions.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="ListExtensions.cs" company="Peter Sulucz">
+// Copyright (c) Peter Sulucz. All rights reserved.
+// </copyright>
+
+namespace Sulucz.Common
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+
+    /// <summary>
+    /// List extensions.
+    /// </summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Converts this enumerable to a readonly collection.
+        /// </summary>
+        /// <typeparam name="T">The type of the collection.</typeparam>
+        /// <param name="self">The enumerable.</param>
+        /// <returns>The readonly collection.</returns>
+        public static IReadOnlyCollection<T> ToReadonlyCollection<T>(this IEnumerable<T> self)
+        {
+            if (self is IReadOnlyCollection<T> already)
+            {
+                return already;
+            }
+
+            return new ReadOnlyCollection<T>(self.ToList());
+        }
+    }
+}
